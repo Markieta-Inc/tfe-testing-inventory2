158e6026094edf08bc9131a4f6868798736a4c1a
Christopher Markieta <markietachristopher@gmail.com>
2019-12-23 14:34:47 -0500

    Automated commit

--- repositories.tf
+++ repositories.tf
@@ -44,3 +44,8 @@ resource "github_repository" "example2" {
   name = "repo2"
   description = "Repository 2"
 }
+
+resource "github_repository" "example2" {
+  name = "repo2"
+  description = "Repository 2"
+}
