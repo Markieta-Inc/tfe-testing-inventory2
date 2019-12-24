d66debb30e138559966314adf791d455c7a149e4
Christopher Markieta <markietachristopher@gmail.com>
2019-12-24 14:11:03 -0500

    Automated commit - Adding repository: MyNewRepo5

--- repositories.tf
+++ repositories.tf
@@ -169,3 +169,8 @@ resource "github_repository" "example2" {
   name = "repo2"
   description = "Repository 2"
 }
+
+resource "github_repository" "MyNewRepo5" {
+  name = "MyNewRepo5"
+  description = "This is a great repo!"
+}
